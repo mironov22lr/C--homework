85949759d808d0ad04e4718b9f7a40c3de2d9bf3
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-01-20 20:43:21 +0300

    Add files via upload

---
+++ Hw10/Program.cs
@@ -0,0 +1,13 @@
+﻿//// Напишите программу, которая принимает на вход трёхзначное число 
+////и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("введите трехзначное число");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x < 1000 && x > 99)
+{
+int two = x / 10;
+int result = two % 10;
+Console.Write(result);
+}
+else
+Console.WriteLine("ввели не трехзначное число");
