7eee4e362f01a1a51ad9ed3bb15c14e85e1c321a
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-01-20 20:47:47 +0300

    Add files via upload

---
+++ hw13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x < 100)
+ {
+    Console.Write("третьей цифры нет");
+ }
+else
+{
+while (x >= 1000)
+{
+    x = x / 10;
+}
+
+if (x < 1000)
+ { 
+    int three = x % 100;
+    int tow = three % 10;
+    Console.Write(tow);
+ }
+ }
