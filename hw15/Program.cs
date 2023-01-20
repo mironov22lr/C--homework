e3b589856a65253151a784fc838bdf6ba32c7f7b
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-01-20 20:50:51 +0300

    Add files via upload

---
+++ hw15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+ Console.Write("Enter number of day"); 
+ int number = Convert.ToInt32(Console.ReadLine()); 
+ if (number >= 1 && number <=5) 
+ {
+     Console.WriteLine("is not Day off");
+ }
+ else if (number==6 || number ==7)
+ {
+     Console.WriteLine ("This is Day Off? Yraaa!");
+ }
+ else 
+ {
+   Console.WriteLine("data entry error");
+ }
