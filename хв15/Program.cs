85b4a534515b3a4d6a128185cc1ef26b1a06a9a5
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-01-21 13:24:37 +0300

    добавление if

---
+++ хв15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+ Console.Write("Enter number of day"); 
+ int number = Convert.ToInt32(Console.ReadLine()); 
+ if (number >= 1 && number <=5) 
+ {
+     Console.WriteLine("is not Day off");
+ }
+ else if (number==6 || number ==7)
+ {
+     Console.WriteLine ("This is Day Off? Yraaa!");
+ }
+ else 
+ {
+   Console.WriteLine("data entry error");
+ }
+// Console.WriteLine ("Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+      //Console.WriteLine ("Введите день недели...");
+     // int dayofweek = int.Parse (Console.ReadLine());
+     // if (dayofweek == 6 || dayofweek == 7) Console.WriteLine ("Ответ: Выходной день.");
+     // else if (dayofweek <= 5) Console.WriteLine ("Ответ: Будний день.");
+     // else if (dayofweek > 7) Console.WriteLine ("Ошибка: В неделе всего 7 дней!");
