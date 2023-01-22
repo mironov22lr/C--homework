3996cc25fb909a0ec759ba3bb23107a5bc6cfd27
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-01-22 19:20:07 +0300

    теперь куб а не квадрат

--- хв23/Program.cs
+++ хв23/Program.cs
@@ -12,7 +12,7 @@ class Program
         Console.WriteLine("Введите число N");
         int n = int.Parse(Console.ReadLine());
         for (int i = 1; i <= n; i++)
-            Console.WriteLine("{0}^2 = {1}", i, i*i);
+            Console.WriteLine("{0}^3 = {1}", i, i*i*i);
         Console.ReadKey(true);
     }
 }
