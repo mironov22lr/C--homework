467bee3b97587ae0ea22ecac322148473e4e5d35
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-01-31 20:24:30 +0300

    Update Program.cs

--- hw38/Program.cs
+++ hw38/Program.cs
@@ -41,3 +41,26 @@ void PrintArray(double[] numbers)
     Console.Write("]");
     Console.WriteLine();
 }
+
+
+//double[] array = new double[15];
+//void MinMaxArray(double[] arr)
+/{
+ // double MinNum= 0;
+  //double MaxNum= 0;
+  
+  //for(int i = 0; i < arr.Length; i++)
+ //{
+   // arr[i] = new Random().Next(-5,25);
+   // Console.Write(arr[i]+" ");
+ //}
+   //for (int i = 1; i < arr.Length; i++)
+ //{
+   // if (arr[i] < MinNum) MinNum = arr[i];
+    //if (arr[i] > MaxNum) MaxNum = arr[i];
+     
+ //}
+   //  Console.WriteLine($"Min number array {MinNum}\nMax number array {MaxNum}");
+     //Console.WriteLine($"{MaxNum - MinNum}");
+//}
+//MinMaxArray(array);
