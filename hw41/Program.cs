8805c6400db14cccefdf67c856902d7d2900b554
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-02-02 16:12:29 +0300

    new hw

---
+++ hw41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+  int CountPositivEnteredNumbers(int countOfNumbers) // первый вариант ввода чисел
+ {
+     Console.WriteLine($"Введите {countOfNumbers} целых чисел...");
+
+     int[] arrayEnteredNumbers = new int [countOfNumbers];
+     int count=0;
+
+     for (int i = 0; i < countOfNumbers; i++)
+     {
+         arrayEnteredNumbers[i] = Convert.ToInt32(Console.ReadLine()); 
+         if(arrayEnteredNumbers[i]> 0)
+         {
+             count++;
+         } 
+
+     }
+
+
+     Console.WriteLine($"Введённые числа: {String.Join(", ", arrayEnteredNumbers)}");
+    
+     return count;
+ }
+ Console.WriteLine(CountPositivEnteredNumbers(10)); 
+
+
+//void EnteredNumbersFromUser() // второй вариант ввода чисел
+ //{
+ //    Console.Write("Введите элементы(через пробел): ");
+
+ //    int[] arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); 
+ 
+ //    int count = 0;
+ //   
+ //    for (int i = 0; i < arr.Length; i++)
+ //    {
+ //        if (arr[i] > 0)
+ //        {
+ //            count++;
+ //        }
+ //    }
+    
+ //    Console.WriteLine($"Кол-во элементов > 0: {count}");
+ //}
+ //EnteredNumbersFromUser();
---
+++ hw43/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//Console.Write("Введите k1: ");
+//double k1 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Введите b1: ");
+//double b1 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Введите k2: ");
+//double k2 = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Введите b2: ");
+//double b2 = Convert.ToDouble(Console.ReadLine());
+ 
+//double x = (b1 - b2)/(k2 - k1);
+//double y = k1 * x + b1;
+ 
+//x = Math.Round(x, 3); // для того что б округлитьб обрезала f3//
+//y = Math.Round(y, 3);
+ 
+//Console.WriteLine($"Пересечение в точке: ({x:f3};{y:f3})");
+
+// Второй вариант: Напишите программу, которая найдёт точку пересечения двух прямых,
+ 
+
+(double x, double y) FindCrossPoint(LineFunction factor1, LineFunction factor2)
+{
+    double x = (factor2.b - factor1.b) / (factor1.k - factor2.k);
+    double y = (factor1.k * x) + factor1.b;
+    return (x, y);
+}
+
+LineFunction GetFactors(string equationNumber)
+{
+    LineFunction factorToFill;
+    Console.Write("Введите значение k{0}:", equationNumber);
+    factorToFill.k = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите значение b{0}:", equationNumber);
+    factorToFill.b = Convert.ToDouble(Console.ReadLine());
+    return factorToFill;
+}
+
+LineFunction equation1, equation2;
+
+Console.Clear();
+Console.WriteLine("Найдём точку пересечения двух прямых,");
+Console.Write("заданных уравнениями ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("y = k1 * x + b1");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write(",");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(" y = k2 * x + b2");
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine();
+
+equation1 = GetFactors("1");
+equation2 = GetFactors("2");
+if (equation1.k == equation2.k)
+{
+    Console.WriteLine("Прямые y={0}*x + {1} и y={2}*x + {3} параллельны.",
+                    equation1.k, equation1.b, 
+                    equation2.k, equation2.b);
+    Console.WriteLine("Точки пересечения нет.");
+}
+else
+{
+    double pointX, pointY;
+    (pointX, pointY) = FindCrossPoint(equation1, equation2);
+    Console.WriteLine();
+
+    Console.WriteLine("Координаты точки пересечения = ({0} , {1}).", Math.Round(pointX, 1), Math.Round(pointY, 1));
+}
+
+public record struct LineFunction // y = k * x + b
+{
+    public double k;
+    public double b;
+}
