a7952ee169bb0f59860ea15e74929ae05a703fc7
mironov22lr <121217153+mironov22lr@users.noreply.github.com>
2023-02-10 20:33:10 +0300

    hw

---
+++ hw56/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.Clear();
+ Console.WriteLine($"\nInput the size Array...:");
+ int m = EnterNumbers("Enter m: ");
+ int n = EnterNumbers("Enter n: ");
+ int Line = EnterNumbers("Enter Range 1 to...ower ");
+
+ int[,] array = new int[m, n];
+ InitArray(array);
+ WriteArray(array);
+
+ int minSumRows = 0;
+ int sumRows = SumRowsElements(array, 0);
+ for (int i = 1; i < array.GetLength(0); i++)
+ {
+   int tempSumRows = SumRowsElements(array, i);
+   if (sumRows > tempSumRows)
+   {
+     sumRows = tempSumRows;
+     minSumRows= i;
+   }
+ }
+
+ Console.WriteLine($"\n{minSumRows+1} - sttring min summ ({sumRows}) elements ");
+
+
+ int SumRowsElements(int[,] array, int i)
+ {
+   int sumRows = array[i,0];
+   for (int j = 1; j < array.GetLength(1); j++)
+ {
+     sumRows += array[i,j];
+ }
+   return sumRows;
+ }
+
+ int EnterNumbers(string enter)
+ {
+   Console.Write(enter);
+   int output = Convert.ToInt32(Console.ReadLine());
+   return output;
+ }
+
+ void InitArray(int[,] array)
+ {
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+       array[i, j] = new Random().Next(i);
+     }
+   }
+ }
+
+ void WriteArray (int[,] array)
+ {
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+       Console.Write(array[i,j] + " ");
+     }
+     Console.WriteLine();
+   }
+ }
---
+++ hw58/Program.cs
@@ -0,0 +1,70 @@
+﻿//  58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+
+ Console.WriteLine($"\nEnter size matrix and range of random numbers:"); // за грамотность прошу прощение
+ int a = EnterNumbers("Enter numbers the rows first matrix: ");
+ int b = EnterNumbers("Enter numbers the columns first matrix(and rows second matrix): ");
+ int c = EnterNumbers("Enter numbers the columns second matrix: ");
+ int Line = EnterNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+ int[,] firstMartrix = new int[a, b];
+ InitArray(firstMartrix);
+ Console.WriteLine($"\nFirst matrix:");
+ WriteArray(firstMartrix);
+
+ int[,] secomdMartrix = new int[b, c];
+ InitArray(secomdMartrix);
+ Console.WriteLine($"\nSecond matrix:");
+ WriteArray(secomdMartrix);
+
+ int[,] resultMatrix = new int[a,c];
+
+ MultMatrix(firstMartrix, secomdMartrix, resultMatrix);
+ Console.WriteLine($"\nMultiplication first matrix on second matrix:");
+ WriteArray(resultMatrix);
+
+ void MultMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+ {
+   for (int i = 0; i < resultMatrix.GetLength(0); i++)
+   {
+     for (int j = 0; j < resultMatrix.GetLength(1); j++)
+     {
+       int sum = 0;
+       for (int k = 0; k < firstMartrix.GetLength(1); k++)
+       {
+         sum += firstMartrix[i,k] * secomdMartrix[k,j];
+       }
+       resultMatrix[i,j] = sum;
+     }
+   }
+ }
+
+ int EnterNumbers(string enter)
+ {
+   Console.Write(enter);
+   int output = Convert.ToInt32(Console.ReadLine());
+   return output;
+ }
+
+ void InitArray(int[,] array)
+ {
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+       array[i, j] = new Random().Next(Line);
+     }
+   }
+ }
+
+ void WriteArray (int[,] array)
+ {
+   for (int i = 0; i < array.GetLength(0); i++)
+   {
+     for (int j = 0; j < array.GetLength(1); j++)
+     {
+       Console.Write(array[i,j] + " ");
+     }
+     Console.WriteLine();
+   }
+ }
